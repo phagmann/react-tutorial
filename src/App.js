7ae95471141de11a8600f327291de7fd0487dbc1
Patrick Hagmann <phagmann1@gmail.com>
2017-05-03 00:57:22 +0000

    added javascript ai code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+// import $ from 'jquery';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2 id="welcome">Play Connect Four!</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Simply click a white egg to slide your piece down! Good Luck :)
         </p>
+        
+        
+
       </div>
     );
   }
 }
+
+
+
+
 
 export default App;
